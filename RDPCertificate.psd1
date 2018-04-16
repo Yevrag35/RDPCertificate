d5d437d86e8ae984daa60ccad9e64c84db669b9b
Unknown <mike.garvey@yevrag35.com>
2018-04-15 21:33:20 -0400

    First Commit

---
+++ RDPCertificate.psd1
@@ -0,0 +1,16 @@
+﻿@{
+    GUID = '04fd9617-2c81-497a-b325-ec02c62653a4'
+    Author = 'Mike Garvey'
+    Description = 'A module for generating and applying certificates for use with Remote Desktop Services on local and remote machines.'
+    Copyright = '© 2018 Yevrag35, LLC.  All rights reserved.'
+    ModuleVersion = '1.0.0'
+    PowerShellVersion = '5.0'
+    NestedModules = @('RDPCertificate.psm1')
+    FunctionsToExport = '*'
+    AliasesToExport = '*'
+    CmdletsToExport = ''
+    FileList = @(
+        'RDPCertificate.psd1',
+        'RDPCertificate.psm1'
+    )
+}
---
+++ RDPCertificate.psm1
@@ -0,0 +1,70 @@
+﻿Function Set-RDPCertificate()
+{
+    [CmdletBinding(PositionalBinding=$false)]
+    [alias("setrdcert")]
+    param
+    (
+        [parameter(Mandatory=$true,ParameterSetName='CreateNewCert')]
+        [switch] $WithNewSelfSignedCertificate
+        ,
+        [parameter(Mandatory=$false,ParameterSetName='CreateNewCert')]
+        [datetime] $ValidUntil = [datetime]::Now.AddYears(2)
+        ,
+        [parameter(Mandatory=$false,ParameterSetName='CreateNewCert')]
+        [ValidateSet("SHA256", "SHA384", "SHA512")]
+        [string] $HashAlgorithm = "SHA256"
+        ,
+        [parameter(Mandatory=$false,ParameterSetName='CreateNewCert')]
+        [ValidateSet(2048, 4096, 8192, 16384)]
+        [int] $KeyLength
+    )
+    DynamicParam
+    {
+        $name = 'SHA1Thumbprint'
+        $dict = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+        $attCol = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+        $props = @{ Mandatory = $true; Position = 0; ValueFromPipelineByPropertyName = $true; ParameterSetName='ExistingCert' }
+        $pAtt = New-Object System.Management.Automation.ParameterAttribute -Property $props
+        $attCol.Add($pAtt)
+        $aAtt = New-Object System.Management.Automation.AliasAttribute("sha1", "thumb", "Thumbprint")
+        $attCol.Add($aAtt)
+        $tprints = (Get-ChildItem Cert:\LocalMachine\My).Thumbprint
+        $valSet = New-Object System.Management.Automation.ValidateSetAttribute($tprints)
+        $attCol.Add($valSet)
+        
+        $rtParam = New-Object System.Management.Automation.RuntimeDefinedParameter(
+            $name, [string], $attCol
+        )
+        $dict.Add($name, $rtParam)
+        return $dict
+    }
+    Begin
+    {
+        if ($PSBoundParameters["SHA1Thumbprint"])
+        {
+            $SHA1Thumbprint = $PSBoundParameters["SHA1Thumbprint"]
+        }
+        else
+        {
+            $opts = @{ Subject = $env:COMPUTERNAME ; NotAfter = $ValidUntil; HashAlgorithm = $HashAlgorithm; KeyLength = $KeyLength }
+            if (![String]::IsNullOrEmpty($env:USERDNSDOMAIN))
+            {
+                $opts.Add("DnsName", $env:COMPUTERNAME, "$(("$env:COMPUTERNAME.$env:USERDNSDOMAIN").ToLower())")
+            }
+            $SHA1Thumbprint = $(New-SelfSignedCertificate @opts
+        }
+    }
+    Process
+    {
+        Set-CimInstance `
+            -Namespace 'root\cimv2\TerminalServices' `
+            -Query 'SELECT * FROM Win32_TSGeneralSetting WHERE TerminalName = "RDP-Tcp"' `
+            -Property @{ SSLCertificateSHA1Hash = $SHA1Thumbprint }
+            
+        if ($EnableFirewallRules)
+        {
+            # I don't use "Set-NetFirewallRule" here because of backwards compatibilty.
+            & netsh.exe advfirewall firewall set rule 'group="Remote Desktop"' new enable=yes > $null
+        }
+    }
+}

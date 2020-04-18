c6decab3f69208aba1bfcd13390d52ae07febfe8
Mike Garvey <mike.garvey@yevrag35.com>
2020-04-17 23:00:46 -0400

    BackToScripts?

---
+++ ScriptModule/GetRDPCertificate.ps1
@@ -0,0 +1,149 @@
+﻿
+
+Function GetCertScriptBlock()
+{
+    {
+        Function CheckStoreForCert([string]$name)
+        {
+            $x509Store = New-Object -TypeName System.Security.Cryptography.X509Certificates.X509Store(
+                $name,
+                [System.Security.Cryptography.X509Certificates.StoreLocation]::LocalMachine
+            )
+
+            $x509Store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadOnly)
+
+            $x509Store.Certificates.Find(
+                [System.Security.Cryptography.X509Certificates.X509FindType]::FindByThumbprint,
+                $cimRdpCert,
+                $false
+            )
+            $x509Store.Close()
+        }
+
+        $exceptions = New-Object -TypeName 'System.Collections.Generic.List[System.Exception]'
+
+        $thumProp = "SSLCertificateSHA1Hash"
+        $cimGetArgs = @{
+            Namespace = "ROOT\cimv2\TerminalServices"
+            ClassName = "Win32_TSGeneralSetting"
+            Filter    = "TerminalName = 'RDP-Tcp'"
+            Property  = $thumProp
+        }
+
+        try
+        {
+            $cimRdp = Get-CimInstance @cimGetArgs -ErrorAction Stop
+            if ($null -ne $cimRdp)
+            {
+                $cimRdpCert = $cimRdp.$thumProp
+            }
+        }
+        catch
+        {
+            $exceptions.Add($_.Exception)
+        }
+
+        if (-not [string]::IsNullOrEmpty($cimRdpCert))
+        {
+            # Check 'My' store first
+            $certCol = CheckStoreForCert -name "My"
+            if ($certCol.Count -gt 0)
+            {
+                $foundCert = $certCol
+                $foundStore = "My"
+            }
+            else
+            {
+
+                $stores = Get-Item Cert:\LocalMachine | ForEach-Object StoreNames
+                foreach ($store in $stores.GetEnumerator().Where( { $_.Key -ne "My" }))
+                {
+                    $storeName = $store.Key
+
+                    try
+                    {
+                        $certCol = CheckStoreForCert -name $storeName
+                    }
+                    catch
+                    {
+                        $exceptions.Add($_.Exception)
+                        $x509Store.Close()
+                        continue
+                    }
+
+                    if ($certCol.Count -gt 0)
+                    {
+                        $foundCert = $certCol
+                        $foundStore = $storeName
+                        $x509Store.Close()
+                        break
+                    }
+                    else
+                    {
+                        $x509Store.Close()
+                        continue
+                    }
+                }
+            }
+        }
+
+
+        [pscustomobject]@{
+            PublishedThumbprint = $cimRdpCert
+            Exists              = $foundCert.Count -gt 0
+            Certificates        = $foundCert
+            StoreName           = $foundStore
+            Exceptions          = $exceptions
+            IsFaulted           = $exceptions.Count -gt 0
+        }
+    }
+}
+Function Get-RDPCertificate()
+{
+    [CmdletBinding(DefaultParameterSetName = "None")]
+    param
+    (
+        [Parameter(Mandatory = $true, Position = 0, ParameterSetName = "ByComputerName")]
+        [string[]] $ComputerName,
+
+        [Parameter(Mandatory = $false, ParameterSetName = "ByComputerName")]
+        [pscredential] $Credential,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "BySession")]
+        [System.Management.Automation.Runspaces.PSSession] $Session
+    )
+
+    $selectProps = [System.Collections.Generic.List[object]]@(
+        @{L = "Certificate"; E = { $_.Certificates | Select-Object -First 1 } },
+        "Exceptions",
+        "Exists",
+        "IsFaulted",
+        "PublishedThumbprint"
+        "StoreName"
+    )
+
+    if ($PSCmdlet.ParameterSetName -like "By*")
+    {
+        $sesArgs = @{ }
+        if ($PSBoundParameters.ContainsKey("Session"))
+        {
+            $sesArgs.Add("Session", $Session)
+        }
+        else
+        {
+            $sesArgs.Add("ComputerName", $ComputerName)
+            if ($PSBoundParameters.ContainsKey("Credential"))
+            {
+                $sesArgs.Add("Credential", $Credential)
+            }
+        }
+
+        $selectProps.Insert(1, @{L = "ComputerName"; E = "PSComputerName" })
+
+        Invoke-Command @sesArgs -ScriptBlock (GetCertScriptBlock) | Select-Object $selectProps
+    }
+    else
+    {
+        $(GetCertScriptBlock).Invoke()
+    }
+}
---
+++ ScriptModule/RDPCertificateResult.ps1
@@ -0,0 +1,79 @@
+﻿$code = @'
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Security.Cryptography.X509Certificates;
+
+public class RDPCertificateResult
+{
+    private X509Certificate2[] _certs;
+    private Exception[] _exs;
+    private string _store;
+    private string _thumb;
+
+    public X509Certificate2[] Certificates { get { return _certs; } }
+    public bool Exists { get { return _certs != null && _certs.Length > 0; } }
+    public Exception[] Exceptions { get { return _exs; } }
+    public bool IsFaulted { get { return _exs != null && _exs.Length > 0; } }
+    public string PublishedThumbprint { get { return _thumb; } }
+    public string StoreName { get { return _store; } }
+
+    public RDPCertificateResult(PSObject pso)
+    {
+        foreach (PSPropertyInfo prop in pso.Properties)
+        {
+            if (prop.Name == "Certificates" && prop.Value != null)
+            {
+                this.SetCertificates(prop.Value);
+            }
+            else if (prop.Name == "PublishedThumbprint")
+            {
+                _thumb = prop.Value as string;
+            }
+            else if (prop.Name == "StoreName")
+            {
+                _store = prop.Value as string;
+            }
+            else if (prop.Name == "Exceptions")
+            {
+                if (prop.Value is IList list)
+                {
+                    _exs = new Exception[list.Count];
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        _exs[i] = (Exception)list[i];
+                    }
+                }
+                else
+                {
+                    _exs = new Exception[] { };
+                }
+            }
+        }
+    }
+
+    private void SetCertificates(object propVal)
+    {
+        if (propVal is IList list)
+        {
+            _certs = new X509Certificate2[list.Count];
+            for (int i1 = 0; i1 < list.Count; i1++)
+            {
+                _certs[i1] = (X509Certificate2)list[i1];
+            }
+        }
+        else if (propVal is object[] objArr)
+        {
+            _certs = new X509Certificate2[objArr.Length];
+            for (int i2 = 0; i2 < objArr.Length; i2++)
+            {
+                _certs[i2] = (X509Certificate2)objArr[i2];
+            }
+        }
+    }
+}
+
+'@
+
+Add-Type -TypeDefinition $code -Language CSharp -ErrorAction Stop
